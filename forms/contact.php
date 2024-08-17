06117308d164160a7a8a8990e3e8a975d704cbd6
Raul Garcia <raulgq@gmail.com>
2024-08-17 15:10:55 -0600

    send email with native function

--- forms/contact.php
+++ forms/contact.php
@@ -1,34 +1,26 @@
 <?php
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+try {
+  echo 'Message has been sent';
+  exit;
+  $userName = filter_var($_POST['name'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);;
+  $userEmail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+  $userMessage = filter_var($_POST['message'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);;
+  $userSubject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);;
+  $body = "
+    From: ".$userName."
+    <br>
+    Email: ".$userEmail."
+    <br>
+    Message: ".$userMessage."
+    <br>
+    ";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    $headers = 'From: contacus@finenaturalstone.ca' . "\r\n" .
+                'Reply-To: contacus@finenaturalstone.ca'. "\r\n". 
+                'Content-Type: text/html; charset=UTF-8';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+    mail($userEmail, $userSubject, $body, $headers);
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- vendor/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// index.php file inside the vendor directory
-
-header('HTTP/1.0 403 Forbidden');
-echo 'Access Denied';
-exit;
